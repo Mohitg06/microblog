ac995fe647c4040add9590875ce504a80a307598
Mohit Gadkari <mohit.gadkari@teradata.com>
2019-02-19 22:54:11 -0800

    Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Mohit'}
+    posts = [
+        {
+            'author': {'username': 'Vidula'},
+            'body': 'Beautiful day in San Diego!'
+        },
+        {
+            'author': {'username': 'Bob'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
